3002a932a5443fd1acb6d7306e7ab65ef3ff8cf2
lavierose <103140611+lavierose@users.noreply.github.com>
2023-12-10 22:33:52 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -14,6 +14,16 @@ def generate_car_matrix(df)->pd.DataFrame:
     """
     # Write your logic here
 
+df = pd.read_csv('dataset-1.csv')
+def generate_car_matrix(dataset):
+result_df = data.pivot(index='id_1', columns='id_2', values='car')
+result_df.fillna(0, inplace=True)
+result_df.values[[range(len(result_df))], [range(len(result_df))]] = 0
+return result_df
+new_df = generate_car_matrix(df)
+print(new_df)
+
+
     return df
 
 
@@ -28,6 +38,14 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
+df = pd.read_csv('dataset-1.csv')
+def generate_car_matrix(dataset):
+result_df = data.pivot(index='id_1', columns='id_2', values='car')
+result_df.fillna(0, inplace=True)
+result_df.values[[range(len(result_df))], [range(len(result_df))]] = 0
+return result_df
+new_df = generate_car_matrix(df)
+print(new_df)
 
     return dict()
 
@@ -43,6 +61,11 @@ def get_bus_indexes(df)->list:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
     # Write your logic here
+import pandas as pd
+def get_bus_indexes(df):
+mean_bus = df['bus'].mean()
+indices = df[df['bus'] > 2 * mean_bus].index.tolist()
+return sorted(indices)
 
     return list()
 
@@ -59,6 +82,12 @@ def filter_routes(df)->list:
     """
     # Write your logic here
 
+def filter_routes(df):
+route_avg = df.groupby('route')['truck'].mean()
+routes_greater_than_7 = route_avg[route_avg > 7].index.tolist()
+return sorted(routes_greater_than_7)
+
+
     return list()
 
 
@@ -74,6 +103,12 @@ def multiply_matrix(matrix)->pd.DataFrame:
     """
     # Write your logic here
 
+def multiply_matrix(result_df):
+modified_df = result_df.copy()
+modified_df = modified_df.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25)
+modified_df = modified_df.round(1)
+return modified_df
+
     return matrix
 
 
@@ -88,5 +123,16 @@ def time_check(df)->pd.Series:
         pd.Series: return a boolean series
     """
     # Write your logic here
+    def check_time_completeness(df):
+df['start_timestamp'] = pd.to_datetime(df['startDay'] + ' ' + df['startTime'])
+df['end_timestamp'] = pd.to_datetime(df['endDay'] + ' ' + df['endTime'])
+df['duration'] = df['end_timestamp'] - df['start_timestamp']
+grouped = df.groupby(['id', 'id_2'])
+completeness_check = grouped.apply(lambda x: (
+(x['duration'].min() >= pd.Timedelta(days=1)) and
+(x['duration'].max() <= pd.Timedelta(days=1, seconds=1)) and
+(x['start_timestamp'].dt.dayofweek.nunique() == 7)
+))
+return completeness_check
 
     return pd.Series()
